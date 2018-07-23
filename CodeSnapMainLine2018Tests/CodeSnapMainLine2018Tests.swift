e679bef2b16012e5f912fa1d595fa246ec48087a
Jeff Hyde <scrapy750@gmail.com>
2018-07-22 23:34:33 -0700

    Initial Commit

---
+++ CodeSnapMainLine2018Tests/CodeSnapMainLine2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CodeSnapMainLine2018Tests.swift
+//  CodeSnapMainLine2018Tests
+//
+//  Created by Jeff  Hyde on 7/22/18.
+//  Copyright © 2018 Jeff  Hyde. All rights reserved.
+//
+
+import XCTest
+@testable import CodeSnapMainLine2018
+
+class CodeSnapMainLine2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
