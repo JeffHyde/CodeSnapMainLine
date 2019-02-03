9beb1ff8c42796ee63b0c060e9b9904b617e0dd3
Jeff  Hyde <hydesapps@gmail.com>
2019-02-02 19:38:35 -0800

    Added Readme.

--- CodeSnapMainLine2018Tests/CodeSnapMainLine2018Tests.swift
+++ CodeSnapMainLine2018Tests/CodeSnapMainLine2018Tests.swift
@@ -24,6 +24,8 @@ class CodeSnapMainLine2018Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let text = ResultHeaderHelper.setText(section: 0)
+        XCTAssertEqual(text, "Minimum Pipe Size")
     }
     
     func testPerformanceExample() {
---
+++ Readme.md
@@ -0,0 +1,3 @@
+This app is a residential potable water line calculator using Core Data.
+Avaliable on the Apple App Store 
+https://itunes.apple.com/us/app/code-snap-main-line/id1074179908?mt=8
